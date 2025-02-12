a6ebf5e2840204906e98f6ad3d403d72f8f6b298
Murillou <murillovinicius13@hotmail.com>
2025-02-12 14:10:57 -0300

    feat: adicionada animação ao fechar e abrir o menu responsivo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -8,7 +9,7 @@ export default function Header() {
   };
 
   return (
-    <header className="bg-gray-800 text-white p-4">
+    <header className=" bg-transparent lg:bg-red-700 text-white p-4">
       <div className="flex items-center justify-between">
         <div className="text-xl font-bold">
           <a href="/">nothinglasts4ever</a>
@@ -16,17 +17,17 @@ export default function Header() {
 
         <nav className="hidden lg:flex space-x-6">
           <a href="#home" className="hover:text-gray-400">
-            Design 1
+            Album Artwork
           </a>
+
           <a href="#about" className="hover:text-gray-400">
-            Design 2
+            Personal Work
           </a>
+
           <a href="#services" className="hover:text-gray-400">
-            Design 3
+            Merchandise
           </a>
-          <a href="#portfolio" className="hover:text-gray-400">
-            Design 4
-          </a>
+
           <a href="#blog" className="hover:text-gray-400">
             About me
           </a>
@@ -43,28 +44,33 @@ export default function Header() {
         </button>
       </div>
 
-      {isMenuOpen && (
-        <nav className="lg:hidden mt-4 space-y-4">
-          <a href="#home" className="block hover:text-gray-400">
-            Home
-          </a>
-          <a href="#about" className="block hover:text-gray-400">
-            About
-          </a>
-          <a href="#services" className="block hover:text-gray-400">
-            Services
-          </a>
-          <a href="#portfolio" className="block hover:text-gray-400">
-            Portfolio
-          </a>
-          <a href="#blog" className="block hover:text-gray-400">
-            Blog
-          </a>
-          <a href="#contact" className="block hover:text-gray-400">
-            Contact
-          </a>
-        </nav>
-      )}
+      <AnimatePresence>
+        {isMenuOpen && (
+          <motion.nav
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.3 }}
+            className="flex flex-col justify-center items-center lg:hidden mt-12 space-y-20"
+          >
+            <a href="#about" className="block hover:text-gray-400">
+              Album Artwork
+            </a>
+            <a href="#services" className="block hover:text-gray-400">
+              Personal Work
+            </a>
+            <a href="#portfolio" className="block hover:text-gray-400">
+              Merchandise
+            </a>
+            <a href="#blog" className="block hover:text-gray-400">
+              About me
+            </a>
+            <a href="#contact" className="block hover:text-gray-400">
+              Contact
+            </a>
+          </motion.nav>
+        )}
+      </AnimatePresence>
     </header>
   );
 }
