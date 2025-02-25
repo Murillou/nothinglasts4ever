1518c04908868de06343a66f5e7396fd64c822f2
Murillou <murillovinicius13@hotmail.com>
2025-02-25 09:26:30 -0300

    feat: adiciona páginas Merchandise e Personal Work

---
+++ src/pages/personalwork/index.tsx
@@ -0,0 +1,37 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import PostDesign from '@/assets/AlbumArtwork/postdesign.jpeg';
+import Image from 'next/image';
+import { useEffect } from 'react';
+
+export default function PersonalWork() {
+  useEffect(() => {
+    AOS.init({
+      startEvent: 'DOMContentLoaded',
+      initClassName: 'aos-init',
+      duration: 400,
+      easing: 'ease',
+      once: false,
+    });
+  }, []);
+
+  const images = Array(12).fill(PostDesign);
+
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-4 mx-auto text-[#e0e0e0] px-2 lg:px-20 py-4">
+      {images.map((image, key) => (
+        <div className="cursor-pointer shadow-md transition-all duration-300 hover:shadow-xl hover:scale-105">
+          <Image
+            key={key}
+            src={image}
+            alt=""
+            width={400}
+            height={350}
+            className="rounded-2xl"
+            data-aos="fade-up"
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
