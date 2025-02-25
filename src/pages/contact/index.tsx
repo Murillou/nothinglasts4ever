d74f4a358239e3f4340fe1fd0ad0d93d2c1a07c2
Murillou <murillovinicius13@hotmail.com>
2025-02-25 08:26:27 -0300

    style: estiliza a página contact

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,6 +1,6 @@
 export default function Contact() {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 min-h-screen bg-transparent  text-[#e0e0e0] ">
+    <div className="flex flex-col items-center justify-center gap-4 min-h-[calc(100vh-10rem)] bg-transparent text-[#e0e0e0]">
       <h1 className="text-3xl font-medium text-[#e0e0e0]">Contact me</h1>
 
       <div className="space-y-4 text-center">
