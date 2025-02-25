624f244f560359844978dda37e4a32404911a3c1
Murillou <murillovinicius13@hotmail.com>
2025-02-25 08:27:54 -0300

    feat: adicionar estrutura e estilização da página About Me

---
+++ src/pages/aboutme/index.tsx
@@ -0,0 +1,19 @@
+export default function aboutMe() {
+  return (
+    <div className="flex flex-col justify-center items-center gap-4 min-h-[calc(100vh-10rem)] max-w-2xl mx-auto">
+      <h1 className="text-3xl font-medium text-[#e0e0e0]">About me</h1>
+
+      <div className="break-words">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </div>
+  );
+}
