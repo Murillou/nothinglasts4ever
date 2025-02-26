ad28b23da9cc7056b572f793267b8c1dc7d8662c
Murillou <murillovinicius13@hotmail.com>
2025-02-26 08:09:34 -0300

    feat: adiciona AOS no head do _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,11 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-BR">
+      <Head>
+        <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
